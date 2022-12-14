2bbc0258c1358b6d74ac67184422e027a4cd2d75
master <master@SMLAB408PC15_23>
2022-12-13 19:27:06 -0500

    Agregar archivos de proyecto.

---
+++ App8/Login.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App8
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        private void btnIniciar_Clicked(object sender, EventArgs e)
+        {
+            string usuario = "apasato";
+            string contraseña = "12345";
+            if(usuario == txtUsuario.Text && contraseña == txtContraseña.Text)
+            {
+                Navigation.PushAsync(new Registro(usuario));
+            }
+            else
+            {
+                DisplayAlert("ALERTA", "Uusuario/Contraseña incorrecto", "Cerrar");
+            }
+        }
+    }
+}
